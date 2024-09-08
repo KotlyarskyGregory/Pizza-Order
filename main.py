5b49d6e881e78a3a56a9572073fba3d82ca95e26
KotlyarskyGregory <gregory.kotlyarsky@gmail.com>
2024-09-08 18:39:26 -0400

    Second commit

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ from PIL import ImageTk, Image
 root = tk.Tk()
 root.title("Pizza Order")
 
+# Create entry boxes
+person_name = tk.Entry(root, width=30)
+person_name.grid(row=0, column=1, padx=20, pady=10)
+pizza_type = tk.Entry(root, width=30)
+pizza_type.grid(row=1, column=1, padx=20, pady=10)
+n_slices = tk.Entry(root, width=30)
+n_slices.grid(row=2, column=1, padx=20, pady=10)
 
+# Create text boxes
 root.mainloop()
